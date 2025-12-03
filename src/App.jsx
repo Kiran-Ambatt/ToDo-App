c96def36bf4333cd4b715804d525947f66c86340
unknown <kiranat.487@gmail.com>
2025-12-03 20:08:17 +0530

    implemented react re-render concept using counter increment

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,36 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  console.log("component rendering....");
+  const [count, setCount] = useState(0);
   const [name, setName] = useState(null);
   const [email, setEmail] = useState("");
+
+  useEffect(() => {
+    console.log("useEffect componnet re rendered");
+  });
   /* console.log(name);
   console.log(email); */
-  useEffect(() => {
+  /*  useEffect(() => {
     console.log("the name is", name);
   }, [name]);
 
   useEffect(() => {
     console.log("the email Only once");
-  }, []);
+  }, []); */
 
+  const counter = () => {
+    let newCount = count + 1;
+    setCount(newCount);
+  };
+
+  useEffect(() => {
+    console.log("count changed to:", count);
+  }, [count]);
   return (
     <>
-      <h1>Login</h1>
-      <input
+      <h1>Loginsss</h1>
+      {/* <input
         type="text"
         name=""
         id=""
@@ -37,7 +51,9 @@ function App() {
         onChange={(event) => {
           setEmail(event.target.value);
         }}
-      />
+      /> */}
+      <h1>Count:{count}</h1>
+      <button onClick={counter}>Increment</button>
     </>
   );
 }
