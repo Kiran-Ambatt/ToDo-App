3a7ffd13907a97bb5e77b6d55bf6130bd5f38961
unknown <kiranat.487@gmail.com>
2025-12-04 15:58:15 +0530

    code optimized and  reviewed and added commands

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,42 @@
 import { useEffect, useState } from "react";
-
 import "./App.css";
 
 function App() {
   //initialization------------------
-
-  const [todo, setTodo] = useState("");
-  //let [finalTodoList, setFinalTodoList] = useState(null);
+  const [todo, setTodo] = useState(""); // for storing input variable to state
   const [todoList, setTodoList] = useState(
     JSON.parse(localStorage.getItem("todoList")) || []
-  );
-  console.log("to do text is:", todo);
-  //finalTodoList = JSON.parse(localStorage.getItem("todoList")) || [];
+  ); // for getting the initial item from local storage
 
   //methods---------------------------------------------------
-
   const saveTodo = () => {
-    let todoList = JSON.parse(localStorage.getItem("todoList")) || [];
-    todoList.push(todo);
-    localStorage.setItem("todoList", JSON.stringify(todoList));
-    setTodoList(todoList);
-
-    //setTodo("");
+    // method to store the items to local storage.
+    if (todo != "") {
+      let todoList = JSON.parse(localStorage.getItem("todoList")) || []; //get the item from local storage
+      todoList.push(todo); // push the input to todoList
+      localStorage.setItem("todoList", JSON.stringify(todoList)); //store the list to local storage
+      setTodoList(todoList); // assign the pushed list to state variable
+      setTodo(""); // empty the input field
+    }
   };
-  console.log("to do list", todoList);
-  //useEffects--------------------------------------------------
-
-  /*  useEffect(() => {
-    finalTodoList = JSON.parse(localStorage.getItem("todoList")) || [];
-    setTodoList(finalTodoList);
-  }, []);
- */
 
   return (
     <>
       <h1>To Do App</h1>
       <h3>To Do List:</h3>
-
       <h4>
         <ol>
-          {todoList.map((item, index) => (
+          {todoList.map((item, index /* for printig the list in order */) => (
             <li>{item}</li>
           ))}
         </ol>
       </h4>
-
-      {todoList == "" && <h2>List is empty</h2>}
-
+      {todoList == "" && <h2>List is empty</h2>}{" "}
+      {/* print if the list is empty */}
       <p>Enter the Todo List here:</p>
       <input
         type="text"
-        name=""
-        id=""
+        value={todo}
         placeholder="Type here"
         onChange={(event) => {
           setTodo(event.target.value);
