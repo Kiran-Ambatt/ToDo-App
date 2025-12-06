4dbc0f9eab0820ab86962be78066065b104c6461
unknown <kiranat.487@gmail.com>
2025-12-06 19:14:40 +0530

    added todo list removal functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import "./App.css";
 function App() {
   //initialization------------------
   const [todo, setTodo] = useState(""); // for storing input variable to state
+  const [list, setList] = useState([]);
   const [todoList, setTodoList] = useState([]);
+  const [showList, setShowList] = useState([]);
 
   //useEffect-------------------------------------------------
   useEffect(() => {
@@ -15,10 +17,19 @@ function App() {
   const saveTodo = () => {
     // method to store the items to local storage.
     if (todo != "") {
-      todoList.push(todo); // push the input to todoList
+      todoList.push({ name: todo, id: Date.now() });
+      //todoList.push(list); // push the input to todoList
       localStorage.setItem("todoList", JSON.stringify(todoList)); //store the list to local storage
       setTodo(""); // empty the input field
+      //localStorage.removeItem(qq);
+      console.log(todoList);
     }
+  };
+  const remove = (itemId) => {
+    console.log("remove method called", itemId);
+    let removelist = todoList.filter((task) => task.id !== itemId);
+    console.log("list after removal is", removelist);
+    localStorage.setItem("todoList", JSON.stringify(removelist));
   };
 
   return (
@@ -27,8 +38,11 @@ function App() {
       <h3>To Do List:</h3>
       <h4>
         <ol>
-          {todoList.map((item, index /* for printig the list in order */) => (
-            <li>{item}</li>
+          {todoList.map((item, index) => (
+            <li key={item.id}>
+              {item.name}
+              <button onClick={() => remove(item.id)}>❌</button>
+            </li>
           ))}
         </ol>
       </h4>
@@ -38,6 +52,7 @@ function App() {
       <input
         type="text"
         value={todo}
+        //name="name"
         placeholder="Type here"
         onChange={(event) => {
           setTodo(event.target.value);
