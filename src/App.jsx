14e72908e03def032ffd9ea7ae986b6a9ff0f6db
unknown <kiranat.487@gmail.com>
2025-12-09 23:36:33 +0530

    code refactored as well removed and optimized the code

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,14 @@ import "./App.css";
 
 function App() {
   //initialization------------------
-  const [todo, setTodo] = useState(""); // for storing input variable to state
+  const [todo, setTodo] = useState("");
   const [todoList, setTodoList] = useState([]);
-  let [removelist, setRemovelist] = useState([]);
-  const [show, setShow] = useState("");
 
-  console.log("todo list before removal:", todo);
-
-  //useEffect-------------------------------------------------
   useEffect(() => {
-    let data = JSON.parse(localStorage.getItem("todoList")) || []; //useeffect to get the data from local storage
-    setTodoList(data);
-  }, [todo, show]);
+    // get the data from local storage
+    setTodoList(JSON.parse(localStorage.getItem("todoList")) || []);
+  }, [todo]);
+
   //methods---------------------------------------------------
   const saveTodo = () => {
     // method to store the items to local storage.
@@ -25,15 +21,12 @@ function App() {
       console.log(todoList);
     }
   };
+
   const remove = (itemId) => {
     //method to remove the items from list
-    console.log("remove method called", itemId);
-    removelist = todoList.filter((task) => task.id !== itemId); // using filter method, comparing the existing todo list to the one-
-    //-which passed during calling the remove method and return the remaining list.
-    console.log("list after removal is", removelist);
-    localStorage.setItem("todoList", JSON.stringify(removelist)); //storing the remaining list to local storage.
-    setShow(Date.now()); // this is used to update the state variable "show" by assigning present date so that useEffect will be called-
-    // - each time you call the remove method.here the show variable keep changing
+    const removelist = todoList.filter((task) => task.id !== itemId); // using filter method, comparing the existing todo list to the one-
+    setTodoList(removelist);
+    localStorage.setItem("todoList", JSON.stringify(removelist));
   };
 
   return (
@@ -51,12 +44,10 @@ function App() {
         </ol>
       </h4>
       {todoList == "" && <h2>List is empty</h2>}{" "}
-      {/* print if the list is empty */}
       <p>Enter the Todo List here:</p>
       <input
         type="text"
         value={todo}
-        //name="name"
         placeholder="Type here"
         onChange={(event) => {
           setTodo(event.target.value);
