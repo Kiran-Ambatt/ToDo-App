51ae7a21c820f534cf9404cd71cfb4da9cb5db8d
unknown <kiranat.487@gmail.com>
2025-12-17 20:28:33 +0530

    2nd interview q on print array one by one and if it reaches the end of array then print whole array

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import "./App.css";
+import { useState } from "react";
 import Header from "./Components/Header";
-import Input from "./Components/Input";
-import List from "./Components/List";
 
 function App() {
+  const [number] = useState([5, 6, 7, 8, 9]); // initializing array to a state variable to send it as props
   return (
     <>
-      <Header />
-      <List />
+      <Header num={number} /> {/* passing props to Header component */}
     </>
   );
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,26 @@
-function Header() {
+import { useState } from "react";
+
+function Header({ num }) {
+  const [res, setResult] = useState([]);
+  const [n, setN] = useState(0);
+  console.log("the array is", num);
+
+  const show = () => {
+    if (n < num.length) {
+      // condition to check if the n reached end of an array
+      setResult(num[n]); // set the result to state variable res.
+      let newn = n + 1; // incrementing the value of n for clicking show button
+      setN(newn);
+    } else {
+      setResult(num); //if the n reached end of array then setting the state variable to initial props value. that is the original array
+    }
+  };
+  console.log("number is", res);
   return (
-    <div>
-      <h1>To Do App</h1>
-    </div>
+    <>
+      <button onClick={show}>show</button>
+      <h1>{res}</h1>
+    </>
   );
 }
 export default Header;
