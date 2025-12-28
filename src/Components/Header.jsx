618159fccb0d1fa1c173e408a04c64209a423da0
unknown <kiranat.487@gmail.com>
2025-12-28 12:47:43 +0530

    Added API call

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,13 @@
+import axios from "axios";
+import { useState } from "react";
+
 function Header() {
+  const [data, setData] = useState([]);
+  axios.get("https://localhost:7168/api/Data").then((res) => setData(res.data));
   return (
     <div>
       <h1>To Do App</h1>
+      <h3>from API: {data}</h3>
     </div>
   );
 }
